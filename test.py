25ea0fda120295e227a3d25d59196a7e60d7af15
A01027265 <A01027265@itesm.mx>
2020-10-04 19:21:44 -0500

    Made DFA table and saved onto dfaTable matrix (lines 73-111)

--- test.py
+++ test.py
@@ -45,6 +45,11 @@ def main():
     alphabet.sort()
     states.sort()
 
+    print('Alphabet:')
+    print(alphabet)
+    print('States:')
+    print(states)
+
     # Make NFA delta table
     nfaTable = [ [0 for i in range(0, len(alphabet)) ] for j in range(0, len(states)) ]
     for tupple in nfa:
@@ -56,15 +61,55 @@ def main():
     print('NFA Table:')
     print(nfaTable)
 
-    # dfaTable = [ [0 for i in range(0, len(alphabet)) ] for j in range(0, len(states)) ]
-
     # Saves all possible combinations of states into array
     stateCombinations = []
     for i in range(1,len(states)):
       comb = combinations(states, i)
       stateCombinations += [''.join(i) for i in comb]
+    stateCombinations.insert(0,'0')
     print('State Combinations:')
     print(stateCombinations)
+
+    # Saves stateCombinations as indexes for usage in dfaTable
+    stateCombinationsIndexes = []
+    tmpStates = states.copy()
+    tmpStates.insert(0,'0')
+    for entry in stateCombinations:
+        if len(entry) == 1:
+            stateCombinationsIndexes.append([tmpStates.index(entry)])
+        else:
+            tmpArray = []
+            i = 0
+            while i < len(entry):
+                tmpArray.append(tmpStates.index(entry[i]))
+                i += 1
+            stateCombinationsIndexes.append(tmpArray)
+    print('State Combinations Indexes:')
+    print(stateCombinationsIndexes)
+            
+    # Make NFA delta table
+    dfaTable = []
+    for index, entry in enumerate(stateCombinationsIndexes):
+        if len(entry) == 1:
+            if entry[0] == 0:
+                dfaTable.append([0,0])
+            else:
+                tmpArray = []
+                for alIdx in enumerate(alphabet):
+                    tmpArray.append(nfaTable[index-1][alIdx[0]])
+                dfaTable.append(tmpArray)
+        else:
+            tmpArray = []
+            for alIdx in enumerate(alphabet):
+                tmpString = ""
+                for enIndex in entry:
+                    if dfaTable[enIndex][alIdx[0]] != 0:
+                        tmpString += str(dfaTable[enIndex][alIdx[0]])
+                tmpArray.append(tmpString)
+            dfaTable.append(tmpArray)
+    print('DFA Table:')
+    print(dfaTable)
+
 
 if __name__ == "__main__":
     main()
